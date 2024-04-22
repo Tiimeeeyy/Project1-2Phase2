3becfa1bd7e9118b9329f2e7a3bd375075dd4ca4
Xun <xun.li@student.maastrichtuniversity.nl>
2024-04-23 00:45:00 +0200

    add comments

--- src/main/java/engine/solvers/MySolver.java
+++ src/main/java/engine/solvers/MySolver.java
@@ -1,5 +1,17 @@
 package engine.solvers;
+/**
+ * Interface for numerical solvers for ODEs.
+ */
 public interface MySolver {
+    /**
+     * Solve differential equation give x value at the next time step
+     *
+     * @param f indictate which dynamic function going to be solved, in this game, it is the golf pyhsics
+     * @param x the states of the golf ball for current moment. x={coordinate_x,coordinate_y,velocity_x,velocity_y}
+     * @param a The friction coefficients. a={knetic friction, static friction}
+     * @param dh the height gradient at current coordinate dh={slope_x,slope_y}
+     * @param dt The time step
+     */
     public boolean nextstep(MyFunction f, double[] x,double[] a, double[] dh, double dt);
     
 } 
