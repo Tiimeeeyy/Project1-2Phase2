9133211801c7f1512b3894259962805330e3a7d0
Mona Lisa <MonaLisa@email>
2024-05-10 10:09:09 +0200

    test

--- src/main/java/ui/GUIcontroller.java
+++ src/main/java/ui/GUIcontroller.java
@@ -13,10 +13,10 @@ public class GUIcontroller {
     public void Function(ActionEvent e){
         String function = FunctionTextfield.getText();
 
-        if (!parser.ExpressionParser()){ //TODO: check parser to fix this ifelse statement
-           return;
-        } else System.out.println(function); 
-    }
+        // if (!parser.ExpressionParser()){ //TODO: check parser to fix this ifelse statement
+        //    return;
+        // } else System.out.println(function); 
+    }  
 
     //TODO: Connect main method variables with the four textfields
     //variables accepts vars as str and double so cant do it the same as expression (which is a str)
