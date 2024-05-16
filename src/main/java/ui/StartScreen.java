3a626322b372008e335e3b3fed49860722d175fc
aydin <timur.schmidt@student.maastrichtuniversity.nl>
2024-05-17 01:08:53 +0200

    Added StartScreen.fxml StartScreen.css

---
+++ src/main/java/ui/StartScreen.java
@@ -0,0 +1,19 @@
+package ui;
+
+import javafx.application.Application;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+public class StartScreen extends Application {
+
+    public Text putting;
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
