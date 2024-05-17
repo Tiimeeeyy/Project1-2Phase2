75b61754e4e03983c4f465733f5ac1ec97ed7a31
Tiimeeeyy <timur.schmidt@student.maastrichtuniversity.nl>
2024-05-17 15:39:30 +0200

    Removed Unused Imports and added CSS to FirstScreen.fxml

--- src/main/java/ui/HeightMap3DChart.java
+++ src/main/java/ui/HeightMap3DChart.java
@@ -3,11 +3,7 @@ import javafx.scene.layout.Pane;
 import org.jfree.chart3d.Chart3D;
 import org.jfree.chart3d.Chart3DFactory;
 import org.jfree.chart3d.data.function.Function3D;
-import org.jfree.chart3d.data.function.Function3DUtils;
-import org.jfree.chart3d.data.xyz.XYZDataset;
 import org.jfree.chart3d.fx.Chart3DViewer;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
 
 public class HeightMap3DChart {
     private double[][] heightStorage;
@@ -18,6 +14,10 @@ public class HeightMap3DChart {
         this.root = root;
     }
 
+    /**
+     * This Method takes the inputted height values and creates a 3D plot from them.
+     * @return Chart3DViewer A Chart Object.
+     */
     public Chart3DViewer createChart() {
         // Create a Function3D from heightStorage
         Function3D function = new Function3D() {
@@ -39,6 +39,10 @@ public class HeightMap3DChart {
         // Display the chart in a JavaFX Chart3DViewer
         return new Chart3DViewer(chart);
     }
+
+    /**
+     * This Method Displays the chart and binds it to the height and width constraints of the Pane they're added to.
+     */
     public void display3DChart() {
         Chart3DViewer viewer = createChart();
         viewer.prefWidthProperty().bind(root.widthProperty());
