f3de8f422ef5e8032d899bfd94cd704474bfcca6
Mona Lisa <MonaLisa@email>
2024-05-21 14:35:01 +0200

    added screen factory and bg image

--- src/main/java/ui/controller/StartScreenController.java
+++ src/main/java/ui/controller/StartScreenController.java
@@ -3,14 +3,28 @@ package ui.controller;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
+import javafx.scene.Parent;
 import javafx.stage.Stage;
 import ui.Main;
+import ui.screenFactory.ScreenInterface;
 
-public class StartScreenController {
+public class StartScreenController implements ScreenInterface{
+
+    private Parent root;
+
+    public void setRoot(Parent root) {
+        this.root = root;
+    }
+
+    @Override
+    public Parent getRoot() {
+        return root;
+    }
 
     @FXML
     private void startGame(ActionEvent event) {
-        Main.openGUI();
+        Main mainInst = new Main();
+        mainInst.setScreen("INPUT", "", 0, 0, 0, 0, 0, 0, 0, 0, null, null);
     }
 
     @FXML
---
+++ src/main/java/ui/helpers/HeightMap3DChart.java
@@ -0,0 +1,86 @@
+package ui.helpers;
+
+import javafx.scene.layout.Pane;
+import org.jfree.chart3d.Chart3D;
+import org.jfree.chart3d.Chart3DFactory;
+import org.jfree.chart3d.data.Range;
+import org.jfree.chart3d.data.function.Function3D;
+import org.jfree.chart3d.fx.Chart3DViewer;
+import org.jfree.chart3d.plot.XYZPlot;
+import org.jfree.chart3d.renderer.xyz.SurfaceRenderer;
+
+import java.awt.Color;
+
+public class HeightMap3DChart {
+    private double[][] heightStorage;
+    private Pane root;
+
+    public HeightMap3DChart(double[][] heightStorage, Pane root) {
+        this.heightStorage = heightStorage;
+        this.root = root;
+    }
+
+    /**
+     * This Method takes the inputted height values and creates a 3D plot from them.
+     * @return Chart3DViewer A Chart Object.
+     */
+    public Chart3DViewer createChart() {
+        // Create a Function3D from heightStorage
+        Function3D function = new Function3D() {
+            @Override
+            public double getValue(double x, double y) {
+                int xi = (int) Math.round(x);
+                int yi = (int) Math.round(y);
+                if (xi >= 0 && xi < heightStorage.length && yi >= 0 && yi < heightStorage[0].length) {
+                    return heightStorage[xi][yi];
+                } else {
+                    return Double.NaN;
+                }
+            }
+        };
+
+        // Create a chart with the function
+        Chart3D chart = Chart3DFactory.createSurfaceChart("", "", function, "X", "Y", "Z");
+
+        XYZPlot plot = (XYZPlot) chart.getPlot();
+        SurfaceRenderer renderer = (SurfaceRenderer) plot.getRenderer();
+        SingleColorScale colorScale = new SingleColorScale(Color.GREEN);
+        renderer.setColorScale(colorScale);
+        chart.setChartBoxColor(new Color(255, 255, 255, 0));
+
+        // Display the chart in a JavaFX Chart3DViewer
+        return new Chart3DViewer(chart);
+    }
+
+    /**
+     * This Method Displays the chart and binds it to the height and width constraints of the Pane they're added to.
+     */
+    public void display3DChart() {
+        Chart3DViewer viewer = createChart();
+        viewer.prefWidthProperty().bind(root.widthProperty());
+        viewer.prefHeightProperty().bind(root.heightProperty());
+        Pane chartPane = new Pane(viewer);
+        root.getChildren().add(chartPane);
+    }
+
+    /**
+     * A simple color scale implementation to use a single color for the surface.
+     */
+    private static class SingleColorScale implements org.jfree.chart3d.renderer.ColorScale {
+        private Color color;
+
+        public SingleColorScale(Color color) {
+            this.color = color;
+        }
+
+        @Override
+        public Color valueToColor(double value) {
+            return color;
+        }
+
+        @Override
+        public Range getRange() {
+            return new Range(0, 1);
+        }
+    }
+}
