5154428f975de19ad04b29aee89460cc1ae07901
axel <axel.andreev@student.maastrichtuniversity.nl>
2024-06-19 13:13:13 +0200

    Newton Raphson bot. Not functional though

--- src/main/java/ui/controller/ThirdScreenController.java
+++ src/main/java/ui/controller/ThirdScreenController.java
@@ -1,10 +1,9 @@
 package ui.controller;
 
 import engine.bot.AiBotGAV.AiBotGAV;
-import engine.bot.AibotGA.AiBotGA;
-import engine.bot.AibotGA.AiBotMultiShots;
 // import engine.bot.hillClimbingBot.old.HillClimbingBot;
 import engine.bot.hillClimbingBot.upd.HillClimbingBotNEW;
+import engine.bot.newtonRaphsonBot.NewtonRaphsonBot;
 import engine.bot.rule_based_new.DistanceMeasure;
 import engine.solvers.GolfGameEngine;
 import engine.solvers.Utility;
@@ -28,10 +27,8 @@ import ui.screenFactory.ScreenInterface;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.lang.reflect.Array;
 import java.net.MalformedURLException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Optional;
 import javafx.collections.FXCollections;
 import javafx.concurrent.Task;
@@ -942,7 +939,19 @@ public class ThirdScreenController implements ScreenInterface {
             mediaPlayer.stop();
         }
     }
+
+
+    @FXML
+    private void nrBotPlay(){
+        logEvent("!!--NR bot entered the party (it is slow, be patient)--!!");
+        NewtonRaphsonBot nrBot = new NewtonRaphsonBot(golfGame, BallPosition, HolePostion);
+        ArrayList<double[]> velocities = nrBot.NewtonRaphsonMethod();
+        shots = velocities;
+        ballHitMultiple(0);
+        return;
+    }
     
-
-
+    
 }
+
+
